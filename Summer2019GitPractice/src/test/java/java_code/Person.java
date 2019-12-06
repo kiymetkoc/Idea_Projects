258228b301ca73a9a781099617fdee3f94dca090
Kiymet Koc <kiymetkoc44@gmail.com>
2019-12-06 11:13:03 -0500

    push new changes

--- Summer2019GitPractice/src/test/java/java_code/Person.java
+++ Summer2019GitPractice/src/test/java/java_code/Person.java
@@ -1,4 +1,13 @@
 package java_code;
 
 public class Person {
+     private int age;
+    private String firstName;
+    private String fullName;
+     private String address;
+     private int SSN;
+
+     public Person(int age){
+         this.age=age;
+     }
 }
