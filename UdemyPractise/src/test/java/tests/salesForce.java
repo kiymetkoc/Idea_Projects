c78825c201870523db0a663037a216f03c03235e
Kiymet Koc <kiymetkoc44@gmail.com>
2020-01-08 22:20:46 -0500

    new project from scratch

---
+++ UdemyPractise/src/test/java/tests/salesForce.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import utils.BrowserFactory;
+
+public class salesForce {
+
+    WebDriver driver = BrowserFactory.getDriver("chrome");
+
+
+}
